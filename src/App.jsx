c4822dbb677b794ca970918d54945f3d4153813b
Abhishek <suryaabhishek329@gmail.com>
2024-04-16 22:00:13 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <h1>Hello ji </h1>
+    <h1>My Name is Abhishek Banjare</h1>
+    <h1>Am Software Developer from Banglore</h1>      
+    </>
+  )
+}
+
+export default App
