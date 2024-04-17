5fc33f94c9708346517fee9377046bd010e104a3
Abhishek <suryaabhishek329@gmail.com>
2024-04-17 12:17:46 +0530

    yeah some changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from 'react'
 import './App.css'
 import Header from './component/header/Header'
+import Menu from './component/menu/Menu'
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
       <Header />   
+      <Menu />
     </>
   )
 }
---
+++ src/component/menu/Menu.jsx
@@ -0,0 +1,111 @@
+import React from 'react'
+import "./Menu.css"
+const Menu = () => {
+  return (
+    <div className='menu-container'>
+      {/* <h1>Our Menu</h1> */}
+      <div className='container'>
+         <div className='cart-box' >
+            <div className='A' >
+                <img src='src/component/menu/img/img8.jpg' alt=''/>
+            </div>
+            <div className='B'>
+               <h3>Papperoni Pizza 1</h3>
+               <p>$15.99</p>
+            </div>         
+         </div>
+        <div className='cart-box'>
+            <div className='A'>
+               <img src='src/component/menu/img/img7.jpg' alt=''/>
+            </div>
+            <div className='B'>
+                <h3>Papperoni Pizza 2</h3>
+                <p>$15.99</p>
+            </div>
+       </div>
+        <div className='cart-box'>
+            <div className='A'>
+              <img src='src/component/menu/img/img6.jpg' alt=''/>
+            </div>
+            <div className='B'>
+                <h3>Papperoni Pizza 3</h3>
+                <p>$15.99</p>
+            </div>
+            
+        </div>
+        <div className='cart-box'>
+            <div className='A'>
+               <img src='src/component/menu/img/img5.jpg' alt=''/>
+            </div>
+            <div className='B'>
+                <h3>Papperoni Pizza 4</h3>
+                <p>$15.99</p>
+            </div>
+            
+        </div>
+        <div className='cart-box'>
+            <div className='A'>
+               <img src='src/component/menu/img/img4.jpg' alt=''/>
+            </div>
+            <div className='B'>
+                <h3>Papperoni Pizza 5</h3>
+                <p>$15.99</p>
+            </div>
+        </div>
+        <div className='cart-box'>
+            <div className='A'>
+                <img src='src/component/menu/img/img3.jpg' alt=''/>
+            </div>
+            <div className='B'>
+               <h3>Papperoni Pizza 6</h3>
+               <p>$15.99</p>
+            </div>
+              
+        </div>
+
+        <div className='cart-box'>
+            <div className='A'>
+                <img src='src/component/menu/img/img2.jpg' alt=''/>
+            </div>
+            <div className='B'>
+               <h3>Papperoni Pizza 7</h3>
+               <p>$15.99</p>
+            </div>
+            
+         </div>
+        <div className='cart-box'>
+            <div className='A'>
+               <img src='src/component/menu/img/img8.jpg' alt=''/>
+            </div>
+            <div className='B'>
+                <h3>Papperoni Pizza 8</h3>
+                <p>$15.99</p>
+            </div>
+            
+       </div>
+        <div className='cart-box'>
+            <div className='A'>
+              <img src='src/component/menu/img/img4.jpg' alt=''/>
+            </div>
+            <div className='B'>
+                <h3>Papperoni Pizza 9</h3>
+                <p>$15.99</p>
+            </div>
+            
+        </div>
+        <div className='cart-box'>
+            <div className='A'>
+               <img src='src/component/menu/img/img3.jpg' alt=''/>
+            </div>
+            <div className='B'>
+                <h3>Papperoni Pizza 10</h3>
+                <p>$15.99</p>
+            </div>
+            
+        </div>
+        </div>
+    </div>
+  )
+}
+
+export default Menu
